8e507eb36bf2786bedb848585d2138431da21bb6
Tim Prentis <tprentis@gmail.com>
2011-03-27 15:58:48 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IAILLC::Application.config.secret_token = '234732596159a7e9390b39e46ec84d11245a238ab2587da29552c07386f46aaeb3e1714893f3903be0b00e941a8a2b50b4b74cd2d25ba49915fcbb7073db24f9'
